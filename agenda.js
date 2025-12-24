47a41857756c1a3b9616a4ac265833be1d1faa9d
juanHerrera <portador1051@gmail.com>
2025-12-24 20:50:17 -0300

    Refactoring in file programDate

--- agenda.js
+++ agenda.js
@@ -1,17 +1,17 @@
 export const agenda = [
   {
     validCode: "HSAD1234",
-    day: '01/01/2026',
+    day: "01/01/2026",
     time: "09:00 AM",
     service: "Onicomicosis Treatment",
     firstname: "Clarita",
     lastname: "Gomez",
     telephone: "+56 9 8765 4321",
-    email: "clarita.gomez@example.com"
-  },  
-  { 
+    email: "clarita.gomez@example.com",
+  },
+  {
     validCode: "HSAD1235",
-    day: '31/12/2025',
+    day: "31/12/2025",
     time: "10:00 AM",
     service: "Onicomicosis Treatment",
     firstname: "Alberto",
@@ -21,7 +21,7 @@ export const agenda = [
   },
   {
     validCode: "HSAD1236",
-    day: '29/12/2025',
+    day: "29/12/2025",
     time: "11:00 AM",
     service: "Consultation",
     firstname: "Olga",
@@ -31,76 +31,105 @@ export const agenda = [
   },
   {
     validCode: "HSAD1237",
-    day: '23/12/2025',
+    day: "23/12/2025",
     time: "12:30 PM",
     service: "Diabetic Foot Care",
     firstname: "Ricardo",
     lastname: "Lopez",
     telephone: "+56 9 3456 7890",
-    email: "ricardo.lopez@example.com"
-  }
-]
+    email: "ricardo.lopez@example.com",
+  },
+];
 
 export const specialist = [
   {
     id: 1,
     name: "Dr. Juan Perez",
-    specialty: ['Ortopodologia','Podologia Geriatrica', 'Podologia General'],
+    specialty: ["Ortopodologia", "Podologia Geriatrica", "Podologia General"],
     contact: "+56 9 4567 8901",
     consultation: [
-      { day: "22", month: "12", time: "09:00", available: true, validCode: "HSAD1234" },
+      {
+        day: "22",
+        month: "12",
+        time: "09:00",
+        available: true,
+        validCode: "HSAD1234",
+      },
     ],
-    image:"https://img.freepik.com/foto-gratis/hermosa-joven-doctora-mirando-camara-oficina_1301-7807.jpg?semt=ais_hybrid&w=740&q=80"
+    image:
+      "https://img.freepik.com/foto-gratis/hermosa-joven-doctora-mirando-camara-oficina_1301-7807.jpg?semt=ais_hybrid&w=740&q=80",
   },
   {
     id: 2,
     name: "Dra. Maria Rodriguez",
-    specialty: ['Podologia Pediatrica','Biomecanica', 'Podologia General'],
+    specialty: ["Podologia Pediatrica", "Biomecanica", "Podologia General"],
     contact: "+56 9 5678 9012",
     consultation: [
-      { day: "23", month: "12", time: "10:00", available: false, validCode: "HSAD1235" },
+      {
+        day: "23",
+        month: "12",
+        time: "10:00",
+        available: false,
+        validCode: "HSAD1235",
+      },
     ],
-    image:"https://plus.unsplash.com/premium_photo-1658506671316-0b293df7c72b?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZG9jdG9yfGVufDB8fDB8fHww"
+    image:
+      "https://plus.unsplash.com/premium_photo-1658506671316-0b293df7c72b?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZG9jdG9yfGVufDB8fDB8fHww",
   },
   {
     id: 3,
     name: "Dr. Luis Gomez",
-    specialty: ['Podologia Clinica','Podologia General'],
+    specialty: ["Podologia Clinica", "Podologia General"],
     contact: "+56 9 7890 1234",
     consultation: [
-      { day: "24", month: "12", time: "11:00", available: false, validCode: "HSAD1236" },
+      {
+        day: "24",
+        month: "12",
+        time: "11:00",
+        available: false,
+        validCode: "HSAD1236",
+      },
     ],
-    image: "https://img.freepik.com/fotos-premium/confiado-mi-capacidad-medica-retrato-recortado-atractiva-joven-doctora-pie-brazos-cruzados-oficina_590464-2228.jpg?semt=ais_hybrid&w=740&q=80"
+    image:
+      "https://img.freepik.com/fotos-premium/confiado-mi-capacidad-medica-retrato-recortado-atractiva-joven-doctora-pie-brazos-cruzados-oficina_590464-2228.jpg?semt=ais_hybrid&w=740&q=80",
   },
   {
     id: 4,
     name: "Dr. Carlos Sanchez",
-    specialty: ['Podologia Deportiva','Cirugia Podologica'],
+    specialty: ["Podologia Deportiva", "Cirugia Podologica"],
     contact: "+56 9 6789 0123",
     consultation: [
-      { day: "25", month: "12", time: "12:30", available: false, validCode: "HSAD1237" },
+      {
+        day: "25",
+        month: "12",
+        time: "12:30",
+        available: false,
+        validCode: "HSAD1237",
+      },
     ],
-    image: "https://png.pngtree.com/png-clipart/20250311/original/pngtree-charismatic-doctorsmiling-at-the-camera-doctor-healthcare-workers-photo-png-image_20638229.png"
-  }
-]
+    image:
+      "https://png.pngtree.com/png-clipart/20250311/original/pngtree-charismatic-doctorsmiling-at-the-camera-doctor-healthcare-workers-photo-png-image_20638229.png",
+  },
+];
 
-const storedAgenda = JSON.parse(localStorage.getItem('agenda')) || agenda;
-const storedSpecialist = JSON.parse(localStorage.getItem('specialist')) || specialist;
+const storedAgenda = JSON.parse(localStorage.getItem("agenda")) || agenda;
+const storedSpecialist =
+  JSON.parse(localStorage.getItem("specialist")) || specialist;
 
 const validTypes = {
-  day: 'string',
-  time: 'string',
-  service: 'string',
-  firstname: 'string',
-  lastname: 'string',
-  telephone: 'string',
-  email: 'string'
+  day: "string",
+  time: "string",
+  service: "string",
+  firstname: "string",
+  lastname: "string",
+  telephone: "string",
+  email: "string",
 };
 
 const createValidCode = () => {
-  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const numbers = '0123456789';
-  let code = '';
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+  let code = "";
   for (let i = 0; i < 4; i++) {
     code += letters.charAt(Math.floor(Math.random() * letters.length));
   }
@@ -108,14 +137,11 @@ const createValidCode = () => {
     code += numbers.charAt(Math.floor(Math.random() * numbers.length));
   }
   return code;
-}
-
-
-
+};
 
 const isValidType = (type) => {
   return Object.values(validTypes).includes(type);
-}
+};
 
 export const schedule = (newItem) => {
   for (const key in newItem) {
@@ -124,103 +150,98 @@ export const schedule = (newItem) => {
     }
   }
 
-  
-
   newItem.validCode = createValidCode();
 
   storedAgenda.push(newItem);
-  const specialistIndex = storedSpecialist.findIndex(s => s.id === parseInt(newItem.specialistId));
-  
+  const specialistIndex = storedSpecialist.findIndex(
+    (s) => s.id === parseInt(newItem.specialistId)
+  );
 
   const specialist = storedSpecialist[specialistIndex];
-  
+
   specialist.consultation.push({
-    day: newItem.day.split('-')[2],
-    month: newItem.day.split('-')[1],
+    day: newItem.day.split("-")[2],
+    month: newItem.day.split("-")[1],
     time: newItem.time,
     available: false,
-    validCode: newItem.validCode
+    validCode: newItem.validCode,
   });
 
+  localStorage.setItem("agenda", JSON.stringify(storedAgenda));
+  localStorage.setItem("specialist", JSON.stringify(storedSpecialist));
 
-
-  localStorage.setItem('agenda', JSON.stringify(storedAgenda));
-  localStorage.setItem('specialist', JSON.stringify(storedSpecialist));
-  
-
-  return { 
+  return {
     success: true,
-    message: 'Appointment scheduled successfully',
-    validCode: newItem.validCode
+    message: "Appointment scheduled successfully",
+    validCode: newItem.validCode,
   };
-}
+};
 
 export const deleteByValidCode = (validCode) => {
-
-  const index = storedAgenda.findIndex(item => item.validCode === validCode);
+  const index = storedAgenda.findIndex((item) => item.validCode === validCode);
   if (index === -1) {
     return {
       success: false,
-      message: 'No appointment found with the provided valid code'
+      message: "No appointment found with the provided valid code",
     };
   }
   storedAgenda.splice(index, 1);
-  localStorage.setItem('agenda', JSON.stringify(storedAgenda));
-  return {  
+  localStorage.setItem("agenda", JSON.stringify(storedAgenda));
+  return {
     success: true,
-    message: 'Appointment deleted successfully'
+    message: "Appointment deleted successfully",
   };
-}
-
+};
 
 export const removeLast = () => {
   agenda.pop();
-}
+};
 
 export const clear = () => {
   agenda.length = 0;
-} 
+};
 
 export const update = (validCode, updatedItem) => {
-  const index = storedAgenda.findIndex(item => item.validCode === validCode);
+  const index = storedAgenda.findIndex((item) => item.validCode === validCode);
   if (index === -1) {
     return {
       success: false,
-      message: 'No appointment found with the provided valid code'
+      message: "No appointment found with the provided valid code",
     };
   }
   storedAgenda[index] = { ...storedAgenda[index], ...updatedItem };
-  localStorage.setItem('agenda', JSON.stringify(storedAgenda));
-  return {  
+  localStorage.setItem("agenda", JSON.stringify(storedAgenda));
+  return {
     success: true,
-    message: 'Appointment updated successfully'
+    message: "Appointment updated successfully",
   };
-}
+};
 
 export const get = ({ database, specialty }) => {
-  if (database === 'agenda') {
+  if (database === "agenda") {
     return storedAgenda;
-  } else if (database === 'specialties') {
+  } else if (database === "specialties") {
     if (specialty) {
-      console.log('database: speciality', specialty);
-      
-      return specialist.filter(s => s.specialty.includes(specialty));
+      return specialist.filter((s) => s.specialty.includes(specialty));
     }
     return specialist;
-  } else if (database === 'specialist') {
+  } else if (database === "specialist") {
     return storedSpecialist;
   } else {
-    throw new Error('Unknown database'); 
+    throw new Error("Unknown database");
   }
-}
+};
 
 export const getByValidCode = (validCode) => {
-  if (validCode === undefined) {
-    return undefined;
-  }
-
-  const agendaFinded = storedAgenda.find(item => item.validCode === validCode)
-  console.log(agendaFinded ? agendaFinded : null);
+  if (!validCode) return null;
   
-  return agendaFinded ? agendaFinded : 'null';
-}
+  const agendaFinded = storedAgenda.find(
+    (item) => item.validCode === validCode
+  );
+
+  return agendaFinded ? agendaFinded : null;
+};
+
+export const getSpecialistById = ({ id }) => {
+  return storedSpecialist.find((s) => s.id === Number(id));
+};
--- src/html/appointmentInfo/main.js
+++ src/html/appointmentInfo/main.js
@@ -1,25 +1,52 @@
-import { getByValidCode, get, update } from "../../../agenda.js";
-const code = sessionStorage.getItem("appointmentCode");
-const specialist = get({ database: "specialist" });
+import { getByValidCode, getSpecialistById } from "../../../agenda.js";
+const APPOINTMENT_CODE_KEY = "appointmentCode";
 const appointmentInfoContainer = document.getElementById(
   "appointmentInfoContainer"
 );
 
+function loadAppointmentInfo() {
+  const code = getStoredCode();
 
-document.addEventListener("DOMContentLoaded", () => {
   if (!code) {
-    sessionStorage.setItem("appointmentCode", "undefined");
-  } else {
-    createPaymentAlert(code)
+    renderForm();
+    bindCodeForm();
+    return;
   }
 
+  const appointment = getByValidCode(code);
 
-});
+  if (!appointment) {
+    renderForm();
+    bindCodeForm();
+    return;
+  }
 
-function loadAppointmentInfo(code) {
-  
-  if (code === "undefined") {
-    appointmentInfoContainer.innerHTML = `
+  const specialist = getSpecialistById({ id: appointment.specialistId });
+
+  renderAppointmentDetails(appointment, specialist);
+  createPaymentAlert(appointment);
+}
+
+const createPaymentAlert = (appointment) => {
+  if (appointment.pay === false) {
+    /*
+        create a modal alert at the top of the body to notify the user that their appointment is pending payment
+      */
+
+    document.body.insertAdjacentHTML(
+      "afterbegin",
+      `
+        <div class="alert alert-warning alert-dismissible fade show m-0" role="alert">
+          <strong>Atención!</strong> Tu cita está pendiente de pago. Por favor, realiza el <a href="../payment/index.html">pago</a> para confirmar tu cita.
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        `
+    );
+  }
+};
+
+const renderForm = () => {
+  appointmentInfoContainer.innerHTML = `
       <div class="card">
         <div class="card-body">
           <form id="appointmentCodeForm">
@@ -32,20 +59,29 @@ function loadAppointmentInfo(code) {
         </div>
       </div>
     `;
-  } else if (code) {
-    const appointment = getByValidCode(code);
-    console.log(appointment);
-    
-    appointmentInfoContainer.innerHTML = `
+};
+
+const bindCodeForm = () => {
+  document
+    .querySelector("#appointmentCodeForm")
+    ?.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const code = e.target.querySelector("input").value.trim();
+      const appointment = getByValidCode(code);
+
+      if (!appointment) return;
+
+      
+      setStoredCode(appointment.validCode);
+      loadAppointmentInfo();
+    });
+};
+
+const renderAppointmentDetails = (appointment, specialist) => {
+  appointmentInfoContainer.innerHTML = `
       <div class="mb-4 position-relative"> 
-        <h5>
-          Información de la cita
-        </h5>
-        <p><strong>
-          ID de validación:
-        </strong> ${
-          appointment.validCode
-        }</p>
+        <h5>Información de la cita</h5>
+        <p><strong>ID de validación:</strong> ${appointment.validCode}</p>
         <div class="position-absolute top-50 end-0 translate-middle-y">
           <img src="../../../public/images/Appointment.png" alt="Appointment Icon" width="50" height="50">
         </div>
@@ -56,16 +92,10 @@ function loadAppointmentInfo(code) {
           <div class="card">
             <div class="card-body">
               <img src="${
-                specialist.find((s) => s.id === parseInt(appointment.specialistId))
-                  .image
+                specialist.image
               }" alt="Specialist Image" class="img-fluid mb-3" width="100" height="50">
-              <p><strong>Nombre:</strong> ${
-                specialist.find((s) => s.id === parseInt(appointment.specialistId)).name
-              }</p>
-              <p><strong>Especialidad:</strong> ${
-                specialist.find((s) => s.id === parseInt(appointment.specialistId))
-                  .specialty
-              }</p>
+              <p><strong>Nombre:</strong> ${specialist.name}</p>
+              <p><strong>Especialidad:</strong> ${specialist.specialty}</p>
             </div>
           </div>
         </div>
@@ -85,11 +115,20 @@ function loadAppointmentInfo(code) {
                   </ul>
                 </div>
               </div>
+              
               <ul class="list-group list-group-flush">
-                <li class="list-group-item"><strong>Nombre:</strong> ${appointment.fullname}</li>
-                <li class="list-group-item"><strong>Edad:</strong> ${appointment.patientAge || "N/A"}</li>
-                <li class="list-group-item"><strong>Teléfono:</strong> ${appointment.telephone}</li>
-                <li class="list-group-item"><strong>Correo electrónico:</strong> ${appointment.email}</li>
+                <li class="list-group-item"><strong>Nombre:</strong> ${
+                  appointment.fullname
+                }</li>
+                <li class="list-group-item"><strong>Edad:</strong> ${
+                  appointment.patientAge || "N/A"
+                }</li>
+                <li class="list-group-item"><strong>Teléfono:</strong> ${
+                  appointment.telephone
+                }</li>
+                <li class="list-group-item"><strong>Correo electrónico:</strong> ${
+                  appointment.email
+                }</li>
               </ul>
               <button class="btn btn-link">Consular ficha</button>
             </div>
@@ -106,46 +145,34 @@ function loadAppointmentInfo(code) {
           </div>
         </div>
       </div>
+    `;
+};
 
-    `;
-  } else {
-    appointmentInfoContainer.innerHTML = `
-      <p>No appointment found for the provided code.</p>
-      <a href="index.html" class="btn btn-primary">Schedule an Appointment</a>   
-    `;
+const renderError = () => {
+  appointmentInfoContainer.innerHTML += `
+    <div class="alert alert-danger mt-4" role="alert">
+      Código de validación inválido. Por favor, inténtalo de nuevo.
+    </div>
+  `;
+
+  setTimeout(() => {
+    clearError();
+  }, 5000);
+};
+
+const clearError = () => {
+  const errorAlert = appointmentInfoContainer.querySelector(
+    ".alert.alert-danger"
+  );
+  if (errorAlert) {
+    errorAlert.remove();
   }
-}
+};
 
-loadAppointmentInfo(code);
+const getStoredCode = () => sessionStorage.getItem(APPOINTMENT_CODE_KEY);
 
-document.querySelector("#appointmentCodeForm")?.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const inputCode = document.getElementById("appointmentCodeInput").value.trim();
-  const appointment = getByValidCode(inputCode);
-  console.log(appointment);
-  
-  sessionStorage.setItem("appointmentCode", appointment.validCode);
-  loadAppointmentInfo(appointment.validCode);
-  createPaymentAlert(appointment.validCode);
-});
+const setStoredCode = (code) => {
+  sessionStorage.setItem(APPOINTMENT_CODE_KEY, code);
+};
 
-
-const createPaymentAlert = (code) => {
-  const appointment = getByValidCode(code);
-    
-    if (appointment.pay === false) {
-      /*
-        create a modal alert at the top of the body to notify the user that their appointment is pending payment
-      */
-
-      document.body.insertAdjacentHTML(
-        "afterbegin",
-        `
-        <div class="alert alert-warning alert-dismissible fade show m-0" role="alert">
-          <strong>Atención!</strong> Tu cita está pendiente de pago. Por favor, realiza el <a href="../payment/index.html">pago</a> para confirmar tu cita.
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        `
-      );
-    }
-}
+loadAppointmentInfo();
